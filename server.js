68e47d27d50b38defb26037ae002849bec814cad
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-10-02 17:41:42 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const expressSession = require('express-session');
+const passport = require('./auth/passport');
+
+const app = express();
+
+app.get('/', (req, res) => {
+   res.send('Homepage')
+});
+
+app.set("view engine", "hbs");
+
+app.use(cookieParser('abracadabra'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSession({
+    secret: 'abracadabra',
+    resave: false,
+    saveUninitialized: false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.static(__dirname + '/views'));
+
+app.use('/', require('./routes/pages'));
+
+app.listen(9000, function () {
+    console.log("Server started on http://localhost:9000");
+});
